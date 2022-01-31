eec01d319e9f9e8fb3d12c29657c9403f8634fd4
AndreasSjo <andreas.sjoden@hotmail.com>
2022-01-31 14:44:23 +0100

    add Student to Dao

--- sti-andreas-nasir-dao/src/main/java/sti/andreas/nasir/dao/stiAndreasNasirDao.java
+++ sti-andreas-nasir-dao/src/main/java/sti/andreas/nasir/dao/stiAndreasNasirDao.java
@@ -1,12 +1,17 @@
 package sti.andreas.nasir.dao;
 
-import sti.andreas.nasir.domain.Person;
+import sti.andreas.nasir.domain.Student;
 import sti.andreas.nasir.domain.Teacher;
 
 public interface stiAndreasNasirDao {
+
     Teacher addTeacher(String firstName, String lastName, int ssNumber, Course course, int hourlyWage);
 
     boolean deleteTeacher(String firstName, String lastName, int ssNumber, Course course, int hourlyWage);
+
+    Student addStudent(String firstName, String lastName, int ssNumber, Course course);
+
+    boolean deleteStudent(String firstName, String lastName, int ssNumber, Course course);
 
 
 }
