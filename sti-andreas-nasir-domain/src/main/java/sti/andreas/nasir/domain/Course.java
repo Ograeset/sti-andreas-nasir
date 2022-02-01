6a628d002464b3c62014c4013b6c1f8eefc1209c
AndreasSjo <andreas.sjoden@hotmail.com>
2022-02-01 14:05:35 +0100

    add course to stiAndreasNasir interface and add constructor to Course

--- sti-andreas-nasir-domain/src/main/java/sti/andreas/nasir/domain/Course.java
+++ sti-andreas-nasir-domain/src/main/java/sti/andreas/nasir/domain/Course.java
@@ -5,6 +5,12 @@ private int credits;
 private Teacher teacher;
 private String courseCode;
 
+    public Course(int credits, Teacher teacher, String courseCode){
+        setcredits(credits);
+        setTeacher(teacher);
+        setCourseCode(courseCode);
+    }
+
     public void setcredits(int credits) {}
     public int credits(){return credits; }
 
