75d79ae1c55fc5505ca47d2782641d2c62797251
AndreasSjo <andreas.sjoden@hotmail.com>
2022-02-01 10:11:05 +0100

    change course to list

--- sti-andreas-nasir-domain/src/main/java/sti/andreas/nasir/domain/Student.java
+++ sti-andreas-nasir-domain/src/main/java/sti/andreas/nasir/domain/Student.java
@@ -1,5 +1,7 @@
 package sti.andreas.nasir.domain;
 
+import java.util.List;
+
 public class Student extends Person {
 
     public Student (String firstName, String lastName, int ssNumber, List<Course> courses){
