91212c69c50c95fc0fafd2951fcb7bc463f1f5bd
AndreasSjo <andreas.sjoden@hotmail.com>
2022-02-03 13:40:40 +0100

    add Main class

---
+++ sti-andreas-nasir-service-impl/src/main/java/sti/andreas/nasir/service/impl/Main.java
@@ -0,0 +1,14 @@
+package sti.andreas.nasir.service.impl;
+
+import sti.andreas.nasir.service.StiService;
+
+public class Main {
+    private StiService stiService;
+
+    public Main(StiService stiService) {
+        this.stiService = stiService;
+    }
+    public static void main(String[] args) {
+
+    }
+}
